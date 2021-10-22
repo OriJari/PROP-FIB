b3a77f82b7ae250884a92bad3e8c0f9427dace3d
Oriol Martí <oriol.marti.jariod@estudiantat.upc.edu>
2021-10-22 12:09:59 +0200

    slope one planting

---
+++ SlopeOne/slopone.java
@@ -0,0 +1,29 @@
+
+public class SlopeOne {
+    /*
+    TENIM:
+        private vector u_val(); //valoracions del usurari u
+
+        private  float u_mean; //mitjana valoracions usuari u
+
+        private vector p_u_val(); // prediccions de valoracions del usuari u
+
+
+    */
+
+    public SlopeOne(){}
+
+    public float des_mean(){
+        //pre: true
+        //post: retorna la desviació mitjana de la valoració de dos items
+        /*
+
+
+         */
+    }
+
+
+
+
+
+}
