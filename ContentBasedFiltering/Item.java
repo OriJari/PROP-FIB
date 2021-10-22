ead464d7ec32c08f3254d5d0603fef2843b47fd8
marc.delgado.sanchez <marc.delgado.sanchez@estudiant.upc.edu>
2021-10-22 11:56:18 +0200

    First thought for Item class (very preliminar)

--- ContentBasedFiltering/Item.java
+++ ContentBasedFiltering/Item.java
@@ -1,8 +1,12 @@
 package com.company;
+import java.util.*;
+import java.io.*;
+
 
 public class Item {
     int ID;
     String[] tags;
+    //Constructoras
     public Item() {
         ID = 0;
         tags = null;
@@ -11,6 +15,7 @@ public class Item {
         ID = id;
         tags = null;
     }
+    //Consultoras
     public int getID() {
         return this.ID;
     }
@@ -18,8 +23,12 @@ public class Item {
         return this.tags;
     }
 
+    public int getNumTags() {
+        return tags.length();
+    }
+    //Modificadoras
     public void addTag(String tag1) {
-        int n = tags.length;
+        int n = tags.length();
         String[] new_tags = new String[n+1];
         int i;
         for (i = 0; i < n; i++) new_tags[i] = tags[i];
@@ -28,6 +37,6 @@ public class Item {
     }
 
     public void setID(int id) {
-        ID = id;
+        this.ID = id;
     }
 }
