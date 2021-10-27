59ab194d15ac79a7f50f293d6a3543911b238e92
Oriol Martí Jariod <oirol.marti.jariod@estudiantat.upc.edu>
2021-10-27 13:33:20 +0200

    27/10 13.33 s1

--- SlopeOne/slopeone.java
+++ SlopeOne/slopeone.java
@@ -19,9 +19,14 @@ public class SlopeOne {
 
     */
 
+
+    Map<User,Map<Item,Float>> map_data;
+    Map<Item,Map<Item,Float>> map_des_mitj;
+    Map<Item,Map<Item,Integer>> mFreqMatrix;
+
     //pre: true
     //post: creadora
-    public SlopeOne(){}
+    public static void SlopeOne(){}
 
     /* dudable metode
     //pre: cardinal del Map<Item, Map<Item, Integer>>, el lengh
@@ -30,12 +35,48 @@ public class SlopeOne {
 
     }
     */
-
     //pre:
     //post:  The preprocessing phase, in which is calculated the difference between all item-item preference values
-    public preprocessat_s1(){
+    private static void desviacio_mitjana(Map<User, Map<Item,Float>> dades){
+        /*
+             private static void buildDifferencesMatrix(Map<User, HashMap<Item, Double>> data) {
+        for (HashMap<Item, Double> user : data.values()) {
+            for (Entry<Item, Double> e : user.entrySet()) {
+                if (!diff.containsKey(e.getKey())) {
+                    diff.put(e.getKey(), new HashMap<Item, Double>());
+                    freq.put(e.getKey(), new HashMap<Item, Integer>());
+                }
+                for (Entry<Item, Double> e2 : user.entrySet()) {
+                    int oldCount = 0;
+                    if (freq.get(e.getKey()).containsKey(e2.getKey())) {
+                        oldCount = freq.get(e.getKey()).get(e2.getKey()).intValue();
+                    }
+                    double oldDiff = 0.0;
+                    if (diff.get(e.getKey()).containsKey(e2.getKey())) {
+                        oldDiff = diff.get(e.getKey()).get(e2.getKey()).doubleValue();
+                    }
+                    double observedDiff = e.getValue() - e2.getValue();
+                    freq.get(e.getKey()).put(e2.getKey(), oldCount + 1);
+                    diff.get(e.getKey()).put(e2.getKey(), oldDiff + observedDiff);
+                }
+            }
+        }
+        for (Item j : diff.keySet()) {
+            for (Item i : diff.get(j).keySet()) {
+                double oldValue = diff.get(j).get(i).doubleValue();
+                int count = freq.get(j).get(i).intValue();
+                diff.get(j).put(i, oldValue / count);
+            }
+        }
+        printData(data);
+    }
+
+
+         */
+
 
     }
+
 
 
 
