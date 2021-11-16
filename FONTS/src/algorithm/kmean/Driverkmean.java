8f5d328caef9e129ac87bd1e49958e92732fd13f
Manel <manel.piera@estudiantat.upc.edu>
2021-11-16 14:39:30 +0100

    NPI

--- FONTS/src/algorithm/kmean/Driverkmean.java
+++ FONTS/src/algorithm/kmean/Driverkmean.java
@@ -1,40 +1,96 @@
 package algorithm.kmean;
 
+import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
+import java.util.Vector;
 
 public class Driverkmean {
-    public void testkmean(){
+    private static Scanner sc;
 
+    public static void testcosineSquaredSimil(){
+        kmean Kmean = new kmean();
+        Map<Integer, Float> user1 = leerUsuario();
+        Map<Integer, Float> user2 = leerUsuario();
+        System.out.println("Resultado: " + Kmean.cosineSquaredSimil(user1, user2));
     }
 
-    public void testcosineSquaredSimil(){
+    public static void testequalClusters(){//si no estan en el miso orden se consideran dos grupos de clusters diferentes
+        System.out.println("Cluster 1:");
+        Vector<Vector<Integer>> clusters1 = leerclusters();
+        System.out.println("Cluster 2:");
+        Vector<Vector<Integer>> clusters2 = leerclusters();
 
+        kmean Kmean = new kmean();
+        boolean equal = Kmean.equalClusters(clusters1, clusters2);
+        if(equal){
+            System.out.println("Son iguales.");
+        }
+        else{
+            System.out.println("Son diferentes.");
+        }
     }
 
-    public void testequalClusters(){
-
-    }
-
-    public void testk_means(){
-
-    }
-
-    private TreeMap<Integer, TreeMap<Integer, Float>> leeropinions(){
+    public static void testk_means(){
+        System.out.println("Escoja una k:");
         Scanner sc = new Scanner(System.in);
+        int k = sc.nextInt();
+        kmean Kmean = new kmean(leeropinions());
+        Vector<Vector<Integer>> clusters = Kmean.k_means(k);
+        System.out.println("Resultado:");
+        for(int i = 0; i < clusters.size(); ++i){
+            System.out.println("Cluster " + i + " :");
+            for(int j = 0; j < clusters.get(i).size(); ++j){
+                System.out.println(clusters.get(i).get(j));
+            }
+        }
+    }
+
+    public static Map<Integer, Float> leerUsuario(){
+        Map<Integer, Float> user = new TreeMap<Integer, Float>();
+        System.out.println("Numero de items valorados por usuario: ");
+        int numItems = sc.nextInt();
+        for(int j = 0; j < numItems; ++j){
+            System.out.println("ID item numero " + (j+1) + " : ");
+            int itemID = sc.nextInt();
+            System.out.println("Valoracion de l'item " + (j+1) + " : ");
+            float itemVal = sc.nextFloat();
+            user.put(itemID, itemVal);
+        }
+        return user;
+    }
+
+    public static Vector<Vector<Integer>> leerclusters(){
+        System.out.println("Numero de clusters del grupo:");
+        int numcl1 = sc.nextInt();
+        Vector<Vector<Integer>> clusters1 = new Vector<Vector<Integer>>();
+        for(int i = 0; i < numcl1; ++i){
+            clusters1.add(new Vector<Integer>());
+            System.out.println("Numero de elementos en cluster " + (i+1) + " del grupo 1:");
+            int numel = sc.nextInt();
+            for(int j = 0; j < numel; ++j){
+                System.out.println("UserID en cluster " + (i+1) + " del grupo 1:");
+                int userID = sc.nextInt();
+                clusters1.get(i).add(userID);
+            }
+        }
+        return clusters1;
+    }
+
+    public static TreeMap<Integer, TreeMap<Integer, Float>> leeropinions(){
         System.out.println("Numero de users: ");
         int Nusers = sc.nextInt();
         TreeMap<Integer, TreeMap<Integer, Float>> opinions = new TreeMap<Integer, TreeMap<Integer, Float>>();
         for(int i = 0; i < Nusers; ++i){
-            System.out.println("ID de user num " + i+1 + " : ");
+            System.out.println("ID de user num " + (i+1) + " : ");
             int userID = sc.nextInt();
             opinions.put(userID, new TreeMap<Integer, Float>());
             System.out.println("Numero de items valorados por usuario " + userID + " : ");
             int numItems = sc.nextInt();
             for(int j = 0; j < numItems; ++j){
-                System.out.println("ID item numero " + j+1 + " : ");
+                System.out.println("ID item numero " + (j+1) + " : ");
                 int itemID = sc.nextInt();
-                System.out.println("Valoracion de l'item " + j+1 + " : ");
+                System.out.println("Valoracion de l'item " + (j+1) + " : ");
                 float itemVal = sc.nextFloat();
                 opinions.get(userID).put(itemID, itemVal);
             }
@@ -42,55 +98,47 @@ public class Driverkmean {
         return opinions;
     }
 
-    public void main(String[] args){
+    public static void main(String[] args){
         System.out.println("Driver Kmean");
         boolean salir = false;
+        sc = new Scanner(System.in);
+
         while (!salir) {
-            try {
-                System.out.println("Escoja una opción:");
-                System.out.println("\t 1) testkmean()");
-                System.out.println("\t 2) testcosineSquaredSimil");
-                System.out.println("\t 3) testequalClusters()");
-                System.out.println("\t 4) testk_means()");
-                System.out.println("\t 0) Salir");
-
-                Scanner sc = new Scanner(System.in);
-                int option = sc.nextInt();
-                switch (option) {
-                    case 1:
-                        try {
-
-                            kmean Kmean = new kmean(leeropinions());
-                            System.out.println("Resultado: " + NN.obtenerPrimeraSolucion(G).toString());
-                        } catch (Exception E) {
-                            System.out.println(E.getMessage());
-                        }
-                    case 2:
-                        try {
-                            int n = Integer.parseInt(line[1]);
-                            G = new Grafo(n);
-                        } catch (Exception E) {
-                            System.out.println(E.getMessage());
-                        }
-                        break;
-                    case 3:
-                        try {
-                            G.ponerArista(new Arista(Integer.parseInt(line[1]), Integer.parseInt(line[2]), Double.parseDouble(line[3])));
-                        } catch (Exception E) {
-                            System.out.println(E.getMessage());
-                        }
-                        break;
-                    case 4:
-                        salir = true;
-                        break;
-                    default:
-                        System.out.println(linea);
-                        break;
-                }
-            } catch (IOException | IOException ex) {
-                System.out.println(ex.getMessage());
+            System.out.println("Escoja una opción:");
+            System.out.println("\t 1) testcosineSquaredSimil()");
+            System.out.println("\t 2) testequalClusters()");
+            System.out.println("\t 3) testk_means()");
+            System.out.println("\t 0) Salir");
+            int option = sc.nextInt();
+            switch (option) {
+                case 1:
+                    try {
+                        testcosineSquaredSimil();
+                    } catch (Exception E) {
+                        System.out.println(E.getMessage());
+                    }
+                    break;
+                case 2:
+                    try {
+                        testequalClusters();
+                    } catch (Exception E) {
+                        System.out.println(E.getMessage());
+                    }
+                    break;
+                case 3:
+                    try {
+                        testk_means();
+                    } catch (Exception E) {
+                        System.out.println(E.getMessage());
+                    }
+                    break;
+                case 0:
+                    salir = true;
+                    break;
+                default:
+                    System.out.println(option);
+                    break;
             }
-
         }
     }
 }
