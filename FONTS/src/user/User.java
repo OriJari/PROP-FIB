7d4c476030ab29e06de9c84dbc2e326228137ddc
miguel.gutierrez.jariod <miguel.gutierrez.jariod@est.fib.upc.edu>
2021-11-17 23:12:58 +0100

    modificacio user 17-11-21

--- FONTS/src/user/User.java
+++ FONTS/src/user/User.java
@@ -4,13 +4,47 @@ import java.util.*;
 import java.io.*;
 
 public class User{
-    String username;
-    int userID;
-    String mail;
-    String password;
+    private String username;
+    private Integer userID;
+    private String mail;
+    private String password;
 
-    //constructoras
-    public User(){
+    public User(String username, Integer userID, String mail, String password){
+        this.username = username;
+        this.userID = userID;
+        this.mail = mail;
+        this.password = password;
+    }
 
+    public String getUsername() {
+        return username;
+    }
+
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
