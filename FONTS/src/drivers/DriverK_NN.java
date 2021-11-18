652ac81f36d2b40d81d0eec8e971a6638132695d
marc.delgado.sanchez <marc.delgado.sanchez@estudiant.upc.edu>
2021-11-18 18:05:47 +0100

    K-NN fully implemented

--- FONTS/src/drivers/DriverK_NN.java
+++ FONTS/src/drivers/DriverK_NN.java
@@ -2,7 +2,6 @@ package drivers;
 
 import algorithm.contentbasedflitering.*;
 import content.Content;
-import item.Item;
 
 import java.io.FileReader;
 import java.io.IOException;
@@ -15,10 +14,38 @@ import java.io.BufferedReader;
  */
 
 public class DriverK_NN {
+
+    static private BufferedReader buffer;
+
+    public static Map<Integer, Map<Integer, Float>> leeropinions() throws IOException {
+        System.out.println("Numero de users: ");
+        String line = buffer.readLine();
+        int num_users = Integer.parseInt(line);
+        Map<Integer, Map<Integer, Float>> opinions = new TreeMap<>();
+        for(int i = 0; i < num_users; ++i){
+            System.out.println("ID de user num " + (i+1) + ":");
+            line = buffer.readLine();
+            int userID = Integer.parseInt(line);
+            opinions.put(userID, new TreeMap<Integer, Float>());
+            System.out.println("Numero de items valorados por usuario " + userID + ":");
+            line = buffer.readLine();
+            int numItems = Integer.parseInt(line);
+            for(int j = 0; j < numItems; ++j){
+                System.out.println("ID item numero " + (j+1) + ":");
+                String line2 = buffer.readLine();
+                int itemID = Integer.parseInt(line2);
+                System.out.println("Valoracion de l'item " + (j+1) + ":");
+                line2 = buffer.readLine();
+                float itemVal = Float.parseFloat(line2);
+                opinions.get(userID).put(itemID, itemVal);
+            }
+        }
+        return opinions;
+    }
     public static void main(String[] args) throws IOException {
         Map<Integer,ArrayList<Content>> map = new TreeMap<>();
         System.out.println("Welcome to Driver Content Based Filtering");
-        BufferedReader buffer = new BufferedReader(new FileReader("C:\\Users\\marcd\\IdeaProjects\\PROP Local\\src\\Input.txt"));
+        buffer = new BufferedReader(new FileReader("FONTS/src/algorithm/contentbasedflitering/Prova.txt"));
         String line;
         System.out.println("How many items will you state?");
         System.out.println("int:");
@@ -54,28 +81,29 @@ public class DriverK_NN {
             }
             map.put(i,lista);
         }
+        Map<Integer,Map<Integer,Float>> map_rate = leeropinions();
         System.out.println("RESULTS:");
-        K_NN taula = new K_NN();
+        K_NN taula = new K_NN(map_rate);
         taula.initSimilarityTable(map);
         System.out.println("Similarity Table:");
         taula.print_similarity_matrix();
         System.out.println("Recommendation Phase:");
         System.out.println("If you wish to terminate execution, type 'end' in any query");
         while (true) {
-            System.out.println("Which item do you want a recommendation for?");
-            System.out.println("input: <Integer> (ranging from 0 to n-1)");
+            System.out.println("Which user do you want a recommendation for?");
+            System.out.println("input: <Integer> ");
             line = buffer.readLine();
             if (line == null || line.equals("end")) break;
-            int item_id = Integer.parseInt(line);
+            int user_id = Integer.parseInt(line);
             System.out.println("How many items would you like to be recommended?");
             System.out.println("input: <Integer> (smaller or equal to n)");
             line = buffer.readLine();
             if (line == null || line.equals("end")) break;
             int k = Integer.parseInt(line);
-            ArrayList<Item> result = taula.kNN(item_id,k);
+            Map<Integer,Float> result = taula.recommend(user_id,k);
             System.out.println("Recommendation:");
-            for (int i = 0; i < k; ++i) {
-                System.out.print(result.get(i).getID() + " ");
+            for (Map.Entry<Integer,Float> entry : result.entrySet()) {
+                System.out.println(entry.getKey() + " " + entry.getValue());
             }
             System.out.println(" ");
         }
