8b2b7475659fd4dc9db870f9fc58bfcb5ab46327
Manel <manel.piera@estudiantat.upc.edu>
2021-11-18 11:13:21 +0100

    refactor

--- FONTS/src/algorithm/collaborativeFiltering/DrivercollaborativeFiltering.java
+++ FONTS/src/algorithm/collaborativeFiltering/DrivercollaborativeFiltering.java
@@ -1,40 +1,59 @@
 package algorithm.collaborativeFiltering;
 
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
+
+import static algorithm.collaborativeFiltering.DrivercollaborativeFiltering.testrecommend;
 
 public class DrivercollaborativeFiltering {
     private static Scanner sc;
 
+    public static TreeMap<Integer, TreeMap<Integer, Float>> leeropinions(){
+        System.out.println("Numero de users: ");
+        int Nusers = sc.nextInt();
+        TreeMap<Integer, TreeMap<Integer, Float>> opinions = new TreeMap<Integer, TreeMap<Integer, Float>>();
+        for(int i = 0; i < Nusers; ++i){
+            System.out.println("ID de user num " + (i+1) + ":");
+            int userID = sc.nextInt();
+            opinions.put(userID, new TreeMap<Integer, Float>());
+            System.out.println("Numero de items valorados por usuario " + userID + ":");
+            int numItems = sc.nextInt();
+            for(int j = 0; j < numItems; ++j){
+                System.out.println("ID item numero " + (j+1) + ":");
+                int itemID = sc.nextInt();
+                System.out.println("Valoracion de l'item " + (j+1) + ":");
+                float itemVal = sc.nextFloat();
+                opinions.get(userID).put(itemID, itemVal);
+            }
+        }
+        return opinions;
+    }
+
+    public static void testrecommend(){
+        TreeMap<Integer, TreeMap<Integer, Float>> opinions = leeropinions();
+        collaborativeFiltering CF = new collaborativeFiltering(opinions, opinions.size()/3);
+        int userID = sc.nextInt();
+        Map<Integer, Float> recommendation = CF.recommend(userID);
+        for(Map.Entry<Integer, Float> entry: recommendation.entrySet()){
+            System.out.println("Expected rating for item " + entry.getKey() + ": " + entry.getValue());
+        }
+    }
+
     public static void main(String[] args){
-        System.out.println("Driver Kmean");
+        System.out.println("Driver collaborativeFiltering");
         boolean salir = false;
         sc = new Scanner(System.in);
 
         while (!salir) {
             System.out.println("Escoja una opción:");
-            System.out.println("\t 1) testcosineSquaredSimil()");
-            System.out.println("\t 2) testequalClusters()");
-            System.out.println("\t 3) testk_means()");
+            System.out.println("\t 1) testrecommend()");
             System.out.println("\t 0) Salir");
             int option = sc.nextInt();
             switch (option) {
                 case 1:
                     try {
-
-                    } catch (Exception E) {
-                        System.out.println(E.getMessage());
-                    }
-                    break;
-                case 2:
-                    try {
-
-                    } catch (Exception E) {
-                        System.out.println(E.getMessage());
-                    }
-                    break;
-                case 3:
-                    try {
-
+                            testrecommend();
                     } catch (Exception E) {
                         System.out.println(E.getMessage());
                     }
--- FONTS/src/evaluation/evaluation.java
+++ FONTS/src/evaluation/evaluation.java
@@ -1,22 +1,22 @@
-package avaluacio;
+package evaluation;
 
 import java.util.*;
 
-/** @class avaluacio
+/** @class evaluation
  * @brief Evaluates the quality of a recommendation.
  */
-public class avaluacio {
-    private List<valoracio> recomanacio;
+public class evaluation {
+    private List<rating> recommendation;
     private Map<Integer, Float> known;
 
-    /** @brief <em>recomanacio</em> represents the recommendation to a user ordered decreasingly by the expected rating.
+    /** @brief <em>recommendation</em> represents the recommendation to a user ordered decreasingly by the expected rating.
      *  <em>known</em> represents the actual ratings from users of some items.
      */
 
-    /** @class valoracio
+    /** @class rating
      * @brief Rating of a user of the item with id <em>id</em> and rating <em>valor</em>.
      */
-    private class valoracio{
+    private class rating {
         int id;
         float valor;
         /** @brief <em>id</em> represents the item id.
@@ -28,16 +28,16 @@ public class avaluacio {
          * @param id Item id.
          * @param valor Item rating.
          */
-        public valoracio(Integer id, Float valor){
+        public rating(Integer id, Float valor){
             this.id = id;
             this.valor = valor;
         }
     }
 
-    /** @brief We use this class to sort our <em>recomanacio</em> list.
+    /** @brief We use this class to sort our <em>recommendation</em> list.
      */
-    private class SortByVal implements Comparator<valoracio>{
-        public int compare(valoracio a, valoracio b){
+    private class SortByVal implements Comparator<rating>{
+        public int compare(rating a, rating b){
             if(a.valor > b.valor){
                 return -1;
             }
@@ -53,12 +53,12 @@ public class avaluacio {
     /** @brief Builder.
      *
      * @param known Sets <em>known</em> to known.
-     * @param recomanacio Sets <em>recomanacio</em> to recomanacio but sorted decreasingly.
+     * @param recommendation Sets <em>recomanacio</em> to recomanacio but sorted decreasingly.
      */
-    public void avaluacio(Map<Integer, Float> known, List<valoracio> recomanacio) {
+    public evaluation(Map<Integer, Float> known, List<rating> recommendation) {
         this.known = known;
-        this.recomanacio = recomanacio;
-        Collections.sort(this.recomanacio, new SortByVal());
+        this.recommendation = recommendation;
+        Collections.sort(this.recommendation, new SortByVal());
     }
 
     /** @brief Function that calculates the quality of a recommendation.
@@ -68,11 +68,11 @@ public class avaluacio {
     float DCG(){
         int j = 1;
         float result = 0.0f;
-        for(int i = 0; i < recomanacio.size(); ++i){
-            if(known.containsKey(recomanacio.get(i).id)){
-                result += (Math.pow(2, known.get(recomanacio.get(i).id))-1)/(Math.log(j + 1)/Math.log(2));
+        for(int i = 0; i < recommendation.size(); ++i){
+            if(known.containsKey(recommendation.get(i).id)){
+                result += (Math.pow(2, known.get(recommendation.get(i).id))-1)/(Math.log(j + 1)/Math.log(2));
             }
-            if(i == 0 || recomanacio.get(i).valor != recomanacio.get(i-1).valor){
+            if(i == 0 || recommendation.get(i).valor != recommendation.get(i-1).valor){
                 ++j;
             }
         }
