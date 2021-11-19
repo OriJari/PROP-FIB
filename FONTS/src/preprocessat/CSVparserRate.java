e472dc2bd1a350e60df57ac5d9b68080940de4ee
miguel.gutierrez.jariod <miguel.gutierrez.jariod@est.fib.upc.edu>
2021-11-19 12:56:45 +0100

    Doxy parser Rate fet

--- FONTS/src/preprocessat/CSVparserRate.java
+++ FONTS/src/preprocessat/CSVparserRate.java
@@ -11,7 +11,7 @@ import java.util.TreeMap;
 
 /**
  * @class CSVparserRate
- * @brief Implements the lecture and the proces of data of the csv
+ * @brief Implements the lecture and the proces of data set of the csv
  * @author Miguel
  */
 
@@ -26,6 +26,11 @@ public class CSVparserRate {
     /**
      * @brief Default builder.
      * @param path where is located the csv document
+     * \pre the path of the file needs to be existent and the document csv needs to be from type ratings.csv
+     *
+     * \post It creates a <em>CSVparserRate</em> object with its attribute <em>path</em> with the value received as a parameter
+     *  ,<em>numCols</em>  initialized as 0, <em>numRows</em>  initialized as 0, <em>content</em> empty, <em>mapRate</em> empty,
+     *   <em>header</em> empty.
      */
     public CSVparserRate(String path){
         this.path = path;
@@ -38,6 +43,8 @@ public class CSVparserRate {
 
     /**
      * @brief Getter of the num rows
+     * \pre the csv document needs to be created and read.
+     * \post obtain the number of the rows of the content attribute.
      * @return the number of rows of the csv
      */
     public Integer getNumRows() {
@@ -46,6 +53,8 @@ public class CSVparserRate {
 
     /**
      * @brief Getter of the num columns
+     * \pre the csv document needs to be created and read.
+     * \post obtain the number of the columns of the content attribute.
      * @return the number of columns of the csv
      */
     public Integer getNumCols() {
@@ -54,6 +63,8 @@ public class CSVparserRate {
 
     /**
      * @brief Getter of the header
+     * \pre the csv document needs to be created and read.
+     * \post obtain the list of the header in the csv.
      * @return the header of the csv as a List
      */
     public List<String> getHeader() {
@@ -62,6 +73,8 @@ public class CSVparserRate {
 
     /**
      * @brief Getter of the path of the location of the csv
+     * \pre needs to be a existent path.
+     * \post obtain the path where is located the csv.
      * @return the path as String
      */
     public String getPath() {
@@ -70,7 +83,9 @@ public class CSVparserRate {
 
     /**
      * @brief Getter of the class, gets the content
-     * @return the lecture of the document csv
+     * \pre the document needs to be read to obtain the content
+     * \post obtain the lecture of the csv ready to manipulate
+     * @return the content value
      */
     public List<List<String>> getContent() {
         return content;
@@ -78,7 +93,9 @@ public class CSVparserRate {
 
     /**
      * @brief Getter of the class, gets the mapRate
-     * @return set of data of the ratings content
+     * \pre the document needs to be read to obtain the content
+     * \post obtain the data set manipulated to make easy the operation in algorithms
+     * @return set of data of the ratings content manipulated
      */
     public Map<Integer, Map<Integer, Float>> getMapRate() {
         return mapRate;
@@ -86,6 +103,8 @@ public class CSVparserRate {
 
     /**
      * @brief Setter of the class, modified the number of rows
+     * \pre true
+     * \post modify the attribute numRows of the class
      * @param numRows, new size of the number of rows
      */
     public void setNumRows(Integer numRows) {
@@ -94,6 +113,8 @@ public class CSVparserRate {
 
     /**
      * @brief Setter of the class, modified the number of columns
+     * \pre true
+     * \post modify the attribute numCols of the class
      * @param numCols, new size of the number of columns
      */
     public void setNumCols(Integer numCols) {
@@ -102,6 +123,8 @@ public class CSVparserRate {
 
     /**
      * @brief Setter of the class, modified the elements of the header
+     * \pre true
+     * \post modify the attribute numRows of the class
      * @param header, header of the csv as a list
      */
     public void setHeader(List<String> header) {
@@ -110,6 +133,8 @@ public class CSVparserRate {
 
     /**
      * @brief Setter of the class, modified the path where is located ths csv
+     * \pre the path needs to be corresponded with the location file
+     * \post modify the attribute path, changing to a new document.
      * @param path, where the document is located, as string
      */
     public void setPath(String path) {
@@ -117,7 +142,9 @@ public class CSVparserRate {
     }
 
     /**
-     * @brief Setter of the class, gets the conent
+     * @brief Setter of the class, gets the content
+     * \pre true
+     * \post modify the attribute content, changing the lecture.
      * @param content, lecture of the csv document to attribute
      */
     public void setContent(List<List<String>> content) {
@@ -126,6 +153,8 @@ public class CSVparserRate {
 
     /**
      * @brief Setter of the class, sets a new mapRate
+     * \pre true
+     * \post modify the attribute mapRate, changing the content of it.
      * @param mapRate , map to redefine the new one
      */
     public void setMapRate(Map<Integer, Map<Integer, Float>> mapRate) {
@@ -135,6 +164,8 @@ public class CSVparserRate {
 
     /**
      * @brief Get the header of the csv.
+     * \pre needs a previous lecture of the csv
+     * \post obtain the set of ids of the document.
      * @param path where the document csv is located
      */
     public void obtainHeader(String path){
@@ -151,6 +182,8 @@ public class CSVparserRate {
 
     /**
      * @brief Read the content of the rates csvs into memory.
+     * \pre needs a document csv to read
+     * \post obtains the lecture of the document manipulated into content.
      */
     public void readLoadRate(){
         FileInputStream fis;
@@ -176,6 +209,8 @@ public class CSVparserRate {
 
     /**
      * @brief Change a String value to an Integer one
+     * \pre true
+     * \post change the value of the string to a integer.
      * @param s String to obtain the corresponding value
      */
     public Integer String_to_Int(String s){
@@ -184,6 +219,8 @@ public class CSVparserRate {
 
     /**
      * @brief Change a String value to a Float one
+     * \pre true
+     * \post change the value of the string to a float.
      * @param s String to obtain the corresponding value
      */
     public Float String_to_Float(String s){
@@ -192,6 +229,8 @@ public class CSVparserRate {
 
     /**
      * @brief Change List<List<String>> to a Map<Integer, Map<Integer,Float>> structure
+     * \pre needs to have a content of the lecture
+     * \post change the content to a estructure where the data is processed
      * @param rate_content content of the csv document
      */
     public void LoadRate(List<List<String>> rate_content){
@@ -213,6 +252,8 @@ public class CSVparserRate {
 
     /**
      * @brief Lecture of the List<List<String>> rows
+     * \pre needs to have a content of the lecture
+     * \post obtain a row of the content read.
      * @param i number of the row to obtain
      */
     public String getRow(int i) {
